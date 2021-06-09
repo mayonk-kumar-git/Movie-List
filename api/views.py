01c7c2686bcb1544e1d4dfcfa589160f61beb372
Mayonk Kumar Behera <mayonk.kumar9@gmail.com>
2021-06-09 13:32:40 +0530

    completed the flask rest api

---
+++ api/views.py
@@ -0,0 +1,27 @@
+from flask import blueprints, jsonify, request
+from . import db
+from .models import Movie
+
+main = blueprints.Blueprint('main', __name__)
+
+
+@main.route('/add_movie', methods=['POST'])
+def add_movie():
+    movie_data = request.get_json()
+    print(movie_data)
+    new_movie = Movie(title=movie_data['title'], rating=movie_data['rating'])
+    db.session.add(new_movie)
+    db.session.commit()
+
+    return 'Done', 201
+
+
+@main.route('/movies')
+def movies():
+    movies_list = Movie.query.all()
+    movies = []
+
+    for movie in movies_list:
+        movies.append({'title': movie.title, 'rating': movie.rating})
+
+    return jsonify({'movies': movies})
